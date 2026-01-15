a04972f5e5d771becfa2f80473965d13ed23a109
Andika Pradnyana <andikapradnyana16@gmail.com>
2026-01-15 12:57:21 +0800

    configuring ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,6 +29,9 @@ export default tseslint.config(
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-floating-promises": "warn",
       "@typescript-eslint/no-unsafe-argument": "warn",
+      "no-unused-var": "warn",
+      "no-console": "off",
+      eqeqeq: "warn",
     },
   },
 );
